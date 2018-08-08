fe1a3791117276b81853c161bd30ee2a3b0bb8f0
ipadorusa <ipadorusa@gmail.com>
2018-08-09 01:00:44 +0900

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,31 @@ const gulp = require('gulp'),
       sass = require('gulp-sass'),
       connect = require('gulp-connect'),
       sourcemaps = require('gulp-sourcemaps'),
-      uglifycss = require('gulp-uglifycss');
+      uglifycss = require('gulp-uglifycss'),
+      plumber = require('gulp-plumber'),
+      autoprefixer = require('gulp-autoprefixer');
+      //inquirer = require('inquirer');
 
 
+const errorHandler = function (error) {
+    console.error(error.message);
+    this.emit('end');
+};
+
+const plumberOption = {
+    errorHandler: errorHandler
+}
 
 gulp.task('sass', function(){
     return gulp.src('./resources/assets/sass/main.scss')
+        .pipe(plumber(plumberOption))
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(uglifycss({
-            "uglyComments": true,
-            "expandVars": true
-        }))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer())
+        // .pipe(uglifycss({
+        //     "uglyComments": true,
+        //     "expandVars": true
+        // }))
         .pipe(sourcemaps.write('/'))
         .pipe(gulp.dest('./public/src/css/'));
 });
@@ -24,6 +37,7 @@ gulp.task('sass:watch', function(){
 
 gulp.task('html', function () {
     gulp.src('./public/src/*.html')
+        .pipe(plumber(plumberOption))
         .pipe(gulp.dest('./public/src/'))
         .pipe(connect.reload());
   });
@@ -43,3 +57,21 @@ gulp.task('watch', function(){
 });
 
 gulp.task('default',['connect', 'sass', 'watch']);
+// gulp.task('default', function (done) {
+// 	inquirer.prompt([
+// 		{
+// 			type: 'list',
+// 			name: 'task',
+// 			message: '어떤 작업을 수행하시겠습니까?',
+// 			choices: [
+// 				{ name: 'JavaScript 빌드', value: 'connect' },
+// 				{ name: 'CSS 빌드', value: 'sass' },
+// 				{ name: 'HTML 빌드', value: 'minifyhtml' },
+// 				new inquirer.Separator(),
+// 				{ name: '전체 빌드', value: 'build' }
+// 			]
+// 		}
+// 	]).then(function (answers) {
+// 		runSequence(answers.task, done);
+// 	});
+// });
