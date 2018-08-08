29bcf469ecd6a9dcb4be348b3fe54bf830a37e1e
ipadorusa <ipadorusa@gmail.com>
2018-08-08 23:00:35 +0900

    study

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    connect = require('gulp-connect'),
-    sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      connect = require('gulp-connect'),
+      sourcemaps = require('gulp-sourcemaps'),
+      uglifycss = require('gulp-uglifycss');
+
+
 
 gulp.task('sass', function(){
-    return gulp.src('./resources/assets/sass/*.scss')
-        //.pipe(sourcemaps.init())
+    return gulp.src('./resources/assets/sass/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        //.pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest('./public/css/'));
+        .pipe(uglifycss({
+            "uglyComments": true,
+            "expandVars": true
+        }))
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest('./public/src/css/'));
 });
 
 gulp.task('sass:watch', function(){
@@ -16,13 +23,14 @@ gulp.task('sass:watch', function(){
 });
 gulp.task('html', function () {
     gulp.src('./public/src/*.html')
-      .pipe(gulp.dest('./app'))
-      .pipe(connect.reload());
+        .pipe(gulp.dest('./public/src/'))
+        .pipe(connect.reload());
   });
 
 // gulp 서버
 gulp.task('connect',function(){
     connect.server({
+        root: './public/src/',
         livereload: true,
         port: 8080
     })
@@ -33,4 +41,4 @@ gulp.task('watch', function(){
     gulp.watch(['./public/src/*.html'], ['html']); // sass 자동 컴파일
 });
 
-gulp.task('default',['connect', 'watch']);
+gulp.task('default',['connect', 'sass', 'watch']);
