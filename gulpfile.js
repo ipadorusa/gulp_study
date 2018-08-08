ad485615727633005ce3cc2005883b12a5b868bb
Kimwoosuk <ipadorusa@gmail.com>
2018-08-08 21:40:36 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,32 @@ var gulp = require('gulp'),
 
 gulp.task('sass', function(){
     return gulp.src('./resources/assets/sass/*.scss')
-        .pipe(sourcemaps.init())
+        //.pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('/'))
+        //.pipe(sourcemaps.write('/'))
         .pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('sass:watch', function(){
     gulp.watch('./resources/assets/sass/*.scss', ['sass']);
 });
+gulp.task('html', function () {
+    gulp.src('./public/src/*.html')
+      .pipe(gulp.dest('./app'))
+      .pipe(connect.reload());
+  });
 
 // gulp 서버
 gulp.task('connect',function(){
     connect.server({
-        root: './public/src',
         livereload: true,
         port: 8080
     })
 });
 
 gulp.task('watch', function(){
-    gulp.watch(['./resources/assets/sass/*.scss'],['sass']) // sass 자동 컴파일
+    gulp.watch(['./resources/assets/sass/*.scss'],['sass']);
+    gulp.watch(['./public/src/*.html'], ['html']); // sass 자동 컴파일
 });
 
 gulp.task('default',['connect', 'watch']);
