525a269be8220e0213dd2601aa46b51da95b8370
ipadorusa <ipadorusa@gmail.com>
2018-08-09 17:42:51 +0900

    update commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,74 @@
 const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      connect = require('gulp-connect'),
-      sourcemaps = require('gulp-sourcemaps'),
-      plumber = require('gulp-plumber'),
-      autoprefixer = require('gulp-autoprefixer');
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    sourcemaps = require('gulp-sourcemaps'),
+    plumber = require('gulp-plumber'),
+    rename = require("gulp-rename"),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
+const paths = {
+    styles: {
+        src: './resources/assets/sass/',
+        dest: './public/src/css/'
+    },
+    scripts: {
+        src: './resources/assets/js/**/',
+        dest: './public/src/js/'
+    },
+    htmls: {
+        src: './public/src/'
+    },
+    root: {
+        src: './public/src/'
+    }
+};
 
-function cssStyle() {
-    return gulp.src('./resources/assets/sass/main.scss')
+function scripts() {
+    return gulp.src(`${paths.scripts.src}*.js`)
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer())
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
         .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest('./public/src/css/'));
+        .pipe(gulp.dest(`${paths.scripts.dest}`))
+        .pipe(connect.reload());
+}
+
+function cssStyle() {
+    return gulp.src(`${paths.styles.src}main.scss`)
+        .pipe(plumber())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(`${paths.styles.dest}`))
+        .pipe(connect.reload());
 }
 
 function htmlReload() {
-    gulp.src('./public/src/*.html')
+    gulp.src(`${paths.htmls.src}**/*.html`)
         .pipe(plumber())
-        .pipe(gulp.dest('./public/src/'))
+        .pipe(gulp.dest(`${paths.htmls.src}`))
         .pipe(connect.reload());
 }
 
 // gulp 서버
 function connectServer() {
     connect.server({
-        root: './public/src/',
+        root: `${paths.root.src}`,
         livereload: true,
         port: 8080
     })
 }
 
 function watch() {
-    gulp.watch('./resources/assets/sass/*.scss',cssStyle);
-    gulp.watch('./public/src/**/*.html', htmlReload); // sass 자동 컴파일
+    gulp.watch(`${paths.styles.src}*.scss`, cssStyle);
+    gulp.watch(`${paths.scripts.src}*.js`,  scripts);
+    gulp.watch(`${paths.htmls.src}**/*.html`, htmlReload);
 }
 
-const build = gulp.parallel(connectServer, cssStyle, watch);
+const build = gulp.parallel(connectServer, cssStyle, scripts, watch);
 
 gulp.task(build);
 gulp.task('default',build);
-
-
-// gulp.task('default', function (done) {
-// 	inquirer.prompt([
-// 		{
-// 			type: 'list',
-// 			name: 'task',
-// 			message: '어떤 작업을 수행하시겠습니까?',
-// 			choices: [
-// 				{ name: 'JavaScript 빌드', value: 'connect' },
-// 				{ name: 'CSS 빌드', value: 'sass' },
-// 				{ name: 'HTML 빌드', value: 'minifyhtml' },
-// 				new inquirer.Separator(),
-// 				{ name: '전체 빌드', value: 'build' }
-// 			]
-// 		}
-// 	]).then(function (answers) {
-// 		runSequence(answers.task, done);
-// 	});
-// });
